fd563db94d78e06b5066884dcc15879adee5f9fd
Suvrajit Maji <44992161+suvrajitm@users.noreply.github.com>
2022-09-10 13:30:50 -0400

    Delete DomainMotionInertiaTensorCode_V2 directory

--- DomainMotionInertiaTensorCode_V2/RunMapSegmentation.tcl
+++
@@ -1,9 +0,0 @@
-## Tcl wrapper script for executing the segmentation program
-## Execute_RunWatershedSegmentMap.sh is the shell script which executes the matlab standalone function for segmentation
-## 
-proc RunMapSegmentation {mcr_root mapFile Gauss_FilterSize walgo pixSize merge_level merge_size extra_merging} {
-
-  exec ./Execute_RunWatershedSegmentMap.sh $mcr_root $mapFile $Gauss_FilterSize $walgo $pixSize $merge_level $merge_size $extra_merging
-  #exec ./Execute_RunWatershedSegmentMap.sh "/guam.raid.cluster.software/matlab2013/" $mapFile $Gauss_FilterSize $walgo $pixSize $merge_depth $extra_merging
-
-}
